e5ea387415170d6bfb06f1d08a69d946c778a250
Ubuntu <ubuntu@ip-172-31-43-38.us-west-2.compute.internal>
2013-07-08 23:28:12 +0000

    added loading data from index.html

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 var buffer = new Buffer(16);
 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var txt = fs.readFileSync('./index.html','utf8');
+  response.send(txt.toString());
 });
 
 var port = process.env.PORT || 5000;
